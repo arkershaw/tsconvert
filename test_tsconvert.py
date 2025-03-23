455b6c331a017e29f1c5a43e7954938d89f12e6c
arkershaw <721253+arkershaw@users.noreply.github.com>
2025-03-23 22:47:20 +0700

    Refactor and improve tests.

--- test_tsconvert.py
+++ test_tsconvert.py
@@ -1,78 +1,282 @@
 import unittest
+import unittest.mock
 from datetime import datetime, timezone
-from tsconvert import UNIX, JAVASCRIPT, CASSANDRA, DOTNET, from_date, to_date, current
 
-TS_DATE = datetime(2018, 3, 3, 12, 0, 0, tzinfo=timezone.utc)
-TS_UNIX = 1520078400
-TS_JAVASCRIPT = 1520078400000
-TS_CASSANDRA = 1520078400000000
-TS_DOTNET = 636556752000000000
+from tsconvert import Precision, TSConverter, DOT_NET_TICKS, UNIX_SECONDS, \
+    from_date_time, to_date_time, current, EPOCH_UNIX, \
+    _parse_args, _DATE_FORMAT_STR
+
+TS_DATE_TIME = datetime(2018, 3, 3, 12, 1, 2, 345678, tzinfo=timezone.utc)
+TS_UNIX = 1520078462
+TS_UNIX_MS = 1520078462345
+TS_UNIX_US = 1520078462345678
+TS_UNIX_NS = 1520078462345678000
+TS_DOT_NET = 636556752623456780
 
 
-class TestTimestamp(unittest.TestCase):
-    def test_from_unix(self):
-        format = UNIX
-        expected = TS_DATE
-        actual = format.to_date(TS_UNIX)
+class TestTSConverter(unittest.TestCase):
+    def test_from_seconds(self):
+        converter = UNIX_SECONDS
+        expected = TS_DATE_TIME.replace(microsecond=0)
+        actual = converter.to_dt(TS_UNIX)
         self.assertTrue(actual.tzinfo == timezone.utc)
         self.assertEqual(expected, actual)
 
-    def test_to_unix(self):
-        format = UNIX
+    def test_to_seconds(self):
+        converter = UNIX_SECONDS
         expected = TS_UNIX
-        actual = format.from_date(TS_DATE)
+        actual = converter.from_dt(TS_DATE_TIME)
         self.assertEqual(expected, actual)
 
-    def test_from_javascript(self):
-        format = JAVASCRIPT
-        expected = TS_DATE
-        actual = format.to_date(TS_JAVASCRIPT)
+    def test_from_milliseconds(self):
+        converter = TSConverter(precision=Precision.MILLISECONDS)
+        expected = TS_DATE_TIME.replace(microsecond=345000)
+        actual = converter.to_dt(TS_UNIX_MS)
         self.assertTrue(actual.tzinfo == timezone.utc)
         self.assertEqual(expected, actual)
 
-    def test_to_javascript(self):
-        format = JAVASCRIPT
-        expected = TS_JAVASCRIPT
-        actual = format.from_date(TS_DATE)
+    def test_to_milliseconds(self):
+        converter = TSConverter(precision=Precision.MILLISECONDS)
+        expected = TS_UNIX_MS
+        actual = converter.from_dt(TS_DATE_TIME)
         self.assertEqual(expected, actual)
 
-    def test_from_cassandra(self):
-        format = CASSANDRA
-        expected = TS_DATE
-        actual = format.to_date(TS_CASSANDRA)
+    def test_from_microseconds(self):
+        converter = TSConverter(precision=Precision.MICROSECONDS)
+        expected = TS_DATE_TIME
+        actual = converter.to_dt(TS_UNIX_US)
         self.assertTrue(actual.tzinfo == timezone.utc)
         self.assertEqual(expected, actual)
 
-    def test_to_cassandra(self):
-        format = CASSANDRA
-        expected = TS_CASSANDRA
-        actual = format.from_date(TS_DATE)
+    def test_to_microseconds(self):
+        converter = TSConverter(precision=Precision.MICROSECONDS)
+        expected = TS_UNIX_US
+        actual = converter.from_dt(TS_DATE_TIME)
+        self.assertEqual(expected, actual)
+
+    def test_from_nanoseconds(self):
+        converter = TSConverter(precision=Precision.NANOSECONDS)
+        expected = TS_DATE_TIME
+        actual = converter.to_dt(TS_UNIX_NS)
+        self.assertTrue(actual.tzinfo == timezone.utc)
+        self.assertEqual(expected, actual)
+
+    def test_to_nanoseconds(self):
+        converter = TSConverter(precision=Precision.NANOSECONDS)
+        expected = TS_UNIX_NS
+        actual = converter.from_dt(TS_DATE_TIME)
         self.assertEqual(expected, actual)
 
     def test_from_dotnet(self):
-        format = DOTNET
-        expected = TS_DATE
-        actual = format.to_date(TS_DOTNET)
+        precision = DOT_NET_TICKS
+        expected = TS_DATE_TIME
+        actual = precision.to_dt(TS_DOT_NET)
         self.assertTrue(actual.tzinfo == timezone.utc)
         self.assertEqual(expected, actual)
 
     def test_to_dotnet(self):
-        format = DOTNET
-        expected = TS_DOTNET
-        actual = format.from_date(TS_DATE)
+        precision = DOT_NET_TICKS
+        expected = TS_DOT_NET
+        actual = precision.from_dt(TS_DATE_TIME)
         self.assertEqual(expected, actual)
 
     def test_from_date(self):
         expected = TS_UNIX
-        actual = from_date(TS_DATE)
+        actual = from_date_time(TS_DATE_TIME)
         self.assertEqual(expected, actual)
 
     def test_to_date(self):
-        expected = TS_DATE
-        actual = to_date(TS_UNIX)
+        expected = TS_DATE_TIME.replace(microsecond=0)
+        actual = to_date_time(TS_UNIX)
         self.assertEqual(expected, actual)
 
     def test_current(self):
         expected = int(datetime.now(timezone.utc).timestamp())
         actual = current()
         self.assertEqual(expected, actual)
+
+
+class TestArgs(unittest.TestCase):
+    def test_no_args(self):
+        expected = datetime.now(timezone.utc)
+        with unittest.mock.patch('sys.argv', ['program_name']), \
+                unittest.mock.patch(
+                    'tsconvert.datetime',
+                    unittest.mock.Mock(now=lambda tz: expected)
+                ):
+            date_time, converters = _parse_args()
+            self.assertEqual(expected, date_time)
+            self.assertEqual(1, len(converters))
+            self.assertEqual(
+                Precision.MICROSECONDS.name.capitalize(), converters[0].name
+            )
+            self.assertEqual(EPOCH_UNIX, converters[0].epoch)
+            self.assertEqual(Precision.MICROSECONDS, converters[0].precision)
+
+    def test_precision(self):
+        expected = datetime.now(timezone.utc)
+        with unittest.mock.patch('sys.argv', ['program_name', '-p', 's']), \
+                unittest.mock.patch(
+                    'tsconvert.datetime',
+                    unittest.mock.Mock(now=lambda tz: expected)
+                ):
+            date_time, converters = _parse_args()
+            self.assertEqual(expected.replace(microsecond=0), date_time)
+            self.assertEqual(1, len(converters))
+            self.assertEqual(
+                Precision.SECONDS.name.capitalize(), converters[0].name
+            )
+            self.assertEqual(EPOCH_UNIX, converters[0].epoch)
+            self.assertEqual(Precision.SECONDS, converters[0].precision)
+
+    def test_alias(self):
+        expected = datetime.now(timezone.utc)
+        with unittest.mock.patch(
+            'sys.argv',
+            ['program_name', '-p', 'javascript']
+            ), \
+            unittest.mock.patch(
+                'tsconvert.datetime',
+                unittest.mock.Mock(now=lambda tz: expected)
+        ):
+            date_time, converters = _parse_args()
+            ms = expected.microsecond // 1000 * 1000
+            self.assertEqual(expected.replace(microsecond=ms), date_time)
+            self.assertEqual(1, len(converters))
+            self.assertEqual(
+                'Javascript', converters[0].name
+            )
+            self.assertEqual(EPOCH_UNIX, converters[0].epoch)
+            self.assertEqual(Precision.MILLISECONDS, converters[0].precision)
+
+    def test_timestamp(self):
+        with unittest.mock.patch(
+            'sys.argv',
+            ['program_name', '-t', str(TS_UNIX_US)]
+        ):
+            date_time, converters = _parse_args()
+            self.assertEqual(TS_DATE_TIME, date_time)
+            self.assertEqual(1, len(converters))
+            self.assertEqual(
+                'Microseconds', converters[0].name
+            )
+            self.assertEqual(EPOCH_UNIX, converters[0].epoch)
+            self.assertEqual(Precision.MICROSECONDS, converters[0].precision)
+
+    def test_timestamp_and_precision(self):
+        with unittest.mock.patch(
+            'sys.argv',
+            ['program_name', '-p', 's', '-t', str(TS_UNIX)]
+        ):
+            date_time, converters = _parse_args()
+            self.assertEqual(TS_DATE_TIME.replace(microsecond=0), date_time)
+            self.assertEqual(1, len(converters))
+            self.assertEqual(
+                'Seconds', converters[0].name
+            )
+            self.assertEqual(EPOCH_UNIX, converters[0].epoch)
+            self.assertEqual(Precision.SECONDS, converters[0].precision)
+
+    def test_date(self):
+        with unittest.mock.patch(
+            'sys.argv',
+            ['program_name', '-d', TS_DATE_TIME.strftime(_DATE_FORMAT_STR)]
+        ):
+            date_time, converters = _parse_args()
+            self.assertEqual(TS_DATE_TIME, date_time)
+            self.assertEqual(1, len(converters))
+            self.assertEqual(
+                'Microseconds', converters[0].name
+            )
+            self.assertEqual(EPOCH_UNIX, converters[0].epoch)
+            self.assertEqual(Precision.MICROSECONDS, converters[0].precision)
+
+    def test_date_and_precision(self):
+        with unittest.mock.patch(
+            'sys.argv',
+            [
+                'program_name', '-p', 's', '-d',
+                TS_DATE_TIME.strftime(_DATE_FORMAT_STR)
+            ]
+        ):
+            date_time, converters = _parse_args()
+            self.assertEqual(TS_DATE_TIME.replace(microsecond=0), date_time)
+            self.assertEqual(1, len(converters))
+            self.assertEqual(
+                'Seconds', converters[0].name
+            )
+            self.assertEqual(EPOCH_UNIX, converters[0].epoch)
+            self.assertEqual(Precision.SECONDS, converters[0].precision)
+
+    def test_invalid_precision(self):
+        with unittest.mock.patch(
+            'sys.argv', ['program_name', '-p', 'x']
+        ):
+            with self.assertRaises(SystemExit):
+                date_time, converters = _parse_args()
+
+    def test_mismatched_precision(self):
+        with unittest.mock.patch(
+            'sys.argv', ['program_name', '-p', 's', '-t', str(TS_UNIX_NS)]
+        ):
+            with self.assertRaisesRegexp(
+                ValueError,
+                r'Timestamp: \d+ too large for precision: Seconds'
+            ):
+                date_time, converters = _parse_args()
+
+    def test_invalid_date(self):
+        with unittest.mock.patch(
+            'sys.argv', [
+                'program_name', '-d',
+                TS_DATE_TIME.strftime('%Y-%m-%d %H:%M:%S')
+            ]
+        ):
+            with self.assertRaises(SystemExit):
+                date_time, converters = _parse_args()
+
+    def test_verbose_timestamp(self):
+        with unittest.mock.patch(
+            'sys.argv',
+            ['program_name', '-vp', 'ns', '-t', str(TS_UNIX_NS)]
+        ):
+            date_time, converters = _parse_args()
+            self.assertEqual(TS_DATE_TIME, date_time)
+            self.assertEqual(5, len(converters))
+            self.assertListEqual([
+                'Seconds',
+                'Milliseconds',
+                'Microseconds',
+                'Nanoseconds',
+                '.NET Ticks',
+            ], [c.name for c in converters])
+
+    def test_verbose_date(self):
+        with unittest.mock.patch(
+            'sys.argv',
+            [
+                'program_name', '-v', '-d',
+                TS_DATE_TIME.strftime(_DATE_FORMAT_STR)
+            ]
+        ):
+            date_time, converters = _parse_args()
+            self.assertEqual(TS_DATE_TIME, date_time)
+            self.assertEqual(5, len(converters))
+            self.assertListEqual([
+                'Seconds',
+                'Milliseconds',
+                'Microseconds',
+                'Nanoseconds',
+                '.NET Ticks',
+            ], [c.name for c in converters])
+
+    def test_date_and_timestamp(self):
+        with unittest.mock.patch(
+            'sys.argv',
+            [
+                'program_name', '-t', str(TS_UNIX), '-d',
+                TS_DATE_TIME.strftime(_DATE_FORMAT_STR)
+            ]
+        ):
+            with self.assertRaises(SystemExit):
+                date_time, converters = _parse_args()
--- tsconvert.py
+++ tsconvert.py
@@ -1,112 +1,210 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from datetime import datetime, timedelta, timezone
 
+__all__ = [
+    'Precision', 'EPOCH_UNIX', 'EPOCH_AD', 'TSConverter', 'UNIX_SECONDS',
+    'DOT_NET_TICKS', 'from_date_time', 'to_date_time', 'current'
+]
+
+from datetime import datetime, timedelta, timezone
+from enum import Enum
+from typing import Dict, Tuple, List
+
+
+class Precision(Enum):
+    SECONDS = 1
+    MILLISECONDS = 1_000
+    MICROSECONDS = 1_000_000
+    DOT_NET_TICKS = 10_000_000
+    NANOSECONDS = 1_000_000_000
+
+
+_DATE_FORMAT_STR = '%Y-%m-%dT%H:%M:%S.%f'
 EPOCH_UNIX = datetime(1970, 1, 1, tzinfo=timezone.utc)
 EPOCH_AD = datetime(1, 1, 1, tzinfo=timezone.utc)
 
-SECONDS = 1
-MILLISECONDS = 2
-MICROSECONDS = 3
-DOTNETTICKS = 4
 
-
-class Timestamp:
-    def __init__(self, epoch=EPOCH_UNIX, precision=SECONDS):
+class TSConverter:
+    def __init__(self,
+                 epoch: datetime = EPOCH_UNIX,
+                 precision: Precision = Precision.SECONDS,
+                 name: str | None = None,
+                 ):
         self.epoch = epoch
         self.precision = precision
-
-    def from_date(self, date: datetime):
-        diff = date.astimezone(timezone.utc) - self.epoch
-        if self.precision == SECONDS:
-            ts = diff / timedelta(seconds=1)
-        elif self.precision == MILLISECONDS:
-            ts = diff / timedelta(milliseconds=1)
-        elif self.precision == MICROSECONDS:
-            ts = diff / timedelta(microseconds=1)
-        elif self.precision == DOTNETTICKS:
-            ts = (diff / timedelta(microseconds=1)) * 10
+        if name is None:
+            self.name = precision.name.capitalize()
         else:
-            raise ValueError('Unknown precision: {0}'.format(self.precision))
-        return int(ts)
+            self.name = name
 
-    def to_date(self, timestamp: int):
-        if self.precision == SECONDS:
-            ts = self.epoch + timedelta(seconds=timestamp)
-        elif self.precision == MILLISECONDS:
-            ts = self.epoch + timedelta(milliseconds=timestamp)
-        elif self.precision == MICROSECONDS:
-            ts = self.epoch + timedelta(microseconds=timestamp)
-        elif self.precision == DOTNETTICKS:
-            ts = self.epoch + timedelta(microseconds=timestamp / 10)
+    def from_dt(self, dt: datetime) -> int:
+        diff = dt.astimezone(timezone.utc) - self.epoch
+        if self.precision == Precision.SECONDS:
+            ts = diff // timedelta(seconds=1)
+        elif self.precision == Precision.MILLISECONDS:
+            ts = diff // timedelta(milliseconds=1)
+        elif self.precision == Precision.MICROSECONDS:
+            ts = diff // timedelta(microseconds=1)
+        elif self.precision == Precision.NANOSECONDS:
+            # No nanoseconds in Python timedelta.
+            ts = diff // timedelta(microseconds=1) * 1000
+        elif self.precision == Precision.DOT_NET_TICKS:
+            ts = diff // timedelta(microseconds=1) * 10
         else:
-            raise ValueError('Unknown precision: {0}'.format(self.precision))
+            raise ValueError(f'Unknown precision: {self.precision}')
         return ts
+
+    def to_dt(self, timestamp: int) -> datetime:
+        try:
+            if self.precision == Precision.SECONDS:
+                ts = self.epoch + timedelta(seconds=timestamp)
+            elif self.precision == Precision.MILLISECONDS:
+                ts = self.epoch + timedelta(milliseconds=timestamp)
+            elif self.precision == Precision.MICROSECONDS:
+                ts = self.epoch + timedelta(microseconds=timestamp)
+            elif self.precision == Precision.NANOSECONDS:
+                ts = self.epoch + timedelta(microseconds=(timestamp // 1_000))
+            elif self.precision == Precision.DOT_NET_TICKS:
+                ts = self.epoch + timedelta(microseconds=timestamp // 10)
+            else:
+                raise ValueError(f'Unknown precision: {self.precision}')
+            return ts
+        except OverflowError:
+            raise ValueError((
+                f'Timestamp: {timestamp} too large '
+                f'for precision: {self.precision.name.capitalize()}'
+            ))
 
 
 # Seconds since 1970-01-01 00:00:00
-UNIX = Timestamp(epoch=EPOCH_UNIX, precision=SECONDS)
-# Milliseconds since 1970-01-01 00:00:00
-JAVASCRIPT = Timestamp(epoch=EPOCH_UNIX, precision=MILLISECONDS)
-# Default is microseconds since 1970-01-01 00:00:00
-CASSANDRA = Timestamp(epoch=EPOCH_UNIX, precision=MICROSECONDS)
+UNIX_SECONDS = TSConverter(EPOCH_UNIX, Precision.SECONDS, 'UNIX Seconds')
 # .NET ticks are ten-millionths of a second since 0001-01-01 00:00:00
-DOTNET = Timestamp(epoch=EPOCH_AD, precision=DOTNETTICKS)
+DOT_NET_TICKS = TSConverter(EPOCH_AD, Precision.DOT_NET_TICKS, '.NET Ticks')
 
 
-def from_date(date: datetime, format: Timestamp = UNIX):
-    return format.from_date(date)
+def from_date_time(
+    date: datetime,
+    converter: TSConverter = UNIX_SECONDS
+) -> int:
+    return converter.from_dt(date)
 
 
-def to_date(timestamp: int, format: Timestamp = UNIX):
-    return format.to_date(timestamp)
+def to_date_time(
+    timestamp: int,
+    converter: TSConverter = UNIX_SECONDS
+) -> datetime:
+    return converter.to_dt(timestamp)
 
 
-def current(format: Timestamp = UNIX):
-    return format.from_date(datetime.now(timezone.utc))
+def current(converter: TSConverter = UNIX_SECONDS) -> int:
+    return converter.from_dt(datetime.now(timezone.utc))
 
 
-def _main():
-    import sys
+def _truncate_date_time(
+    dt: datetime,
+    precision: Precision
+) -> datetime:
+    if precision == Precision.SECONDS:
+        return dt.replace(microsecond=0)
+    elif precision == Precision.MILLISECONDS:
+        ms = dt.microsecond // 1000 * 1000
+        return dt.replace(microsecond=ms)
+    else:
+        return dt
+
+
+def _parse_args() -> Tuple[datetime, List[TSConverter]]:
     import argparse
 
     def validate_date(value):
         try:
-            return datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f').replace(tzinfo=timezone.utc)
+            return (
+                datetime.strptime(value, _DATE_FORMAT_STR)
+                .replace(tzinfo=timezone.utc)
+            )
         except ValueError:
-            raise argparse.ArgumentTypeError('Invalid date/time: {0}.'.format(value))
+            raise argparse.ArgumentTypeError(f'Invalid date/time: {value}.')
 
-    mappings = {
-        'unix': (UNIX, 'UNIX'),
-        'javascript': (JAVASCRIPT, 'Javascript'),
-        'cassandra': (CASSANDRA, 'Cassandra'),
-        'dotnet': (DOTNET, '.NET Ticks')
+    mappings: Dict[str, TSConverter] = {
+        's': TSConverter(EPOCH_UNIX, Precision.SECONDS),
+        'ms': TSConverter(EPOCH_UNIX, Precision.MILLISECONDS),
+        'us': TSConverter(EPOCH_UNIX, Precision.MICROSECONDS),
+        'ns': TSConverter(EPOCH_UNIX, Precision.NANOSECONDS),
     }
 
-    parser = argparse.ArgumentParser(description='Timestamp Converter')
-    parser.add_argument('-f', '--format', dest='format', default='unix', choices=['unix', 'javascript', 'cassandra', 'dotnet'], help='Timestamp input format.')
+    aliases: Dict[str, TSConverter] = {
+        'unix': UNIX_SECONDS,
+        # Javascript timestamps use UNIX epoch and millisecond precision.
+        'javascript': TSConverter(
+            EPOCH_UNIX, Precision.MILLISECONDS, 'Javascript'
+        ),
+        # Cassandra timestamps use UNIX epoch and
+        # microsecond precision by default.
+        'cassandra': TSConverter(
+            EPOCH_UNIX, Precision.MICROSECONDS, 'Cassandra'
+        ),
+        'dotnet': DOT_NET_TICKS,
+    }
+
+    parser = argparse.ArgumentParser(
+        description='Timestamp Converter',
+    )
+    parser.add_argument(
+        '-v',
+        dest='verbose',
+        action='store_true',
+        default=False,
+        help='Output all precisions.'
+    )
+    parser.add_argument(
+        '-p',
+        dest='precision',
+        default='us',
+        choices=list(mappings.keys()) + list(aliases.keys()),
+        help='Timestamp precision.'
+    )
     group = parser.add_mutually_exclusive_group()
-    group.add_argument('-d', '--date', dest='date', type=validate_date, default=None, help='The date and time. Example: 2021-04-30T01:55:01.001')
-    group.add_argument('-t', '--timestamp', dest='timestamp', type=int, default=None, help='The timestamp value.')
+    group.add_argument(
+        '-d',
+        dest='date_time',
+        type=validate_date,
+        default=None,
+        help='The date and time. Example: 2018-03-03T12:00:00.000'
+    )
+    group.add_argument('-t', dest='timestamp', type=int,
+                       default=None, help='The timestamp value.')
     args = parser.parse_args()
 
-    if args.date:
-        dt = args.date
-    elif args.timestamp:
-        if args.format in mappings:
-            format = mappings[args.format][0]
-        else:
-            sys.exit('Unknown format: {0}'.format(args.format))
-        dt = format.to_date(args.timestamp)
+    if args.precision in mappings:
+        converter = mappings[args.precision]
+    elif args.precision in aliases:
+        converter = aliases[args.precision]
     else:
-        dt = datetime.now(timezone.utc)
+        raise ValueError(f'Unknown precision: {args.precision}')
 
-    print('Date (UTC): {0}'.format(dt))
+    if args.date_time:
+        date_time = args.date_time
+    elif args.timestamp:
+        date_time = converter.to_dt(args.timestamp)
+    else:
+        date_time = datetime.now(timezone.utc)
 
-    for format in mappings.values():
-        name = format[1]
-        value = format[0].from_date(dt)
-        print('{0}: {1}'.format(name, value))
+    if args.verbose:
+        converters = list(mappings.values()) + [aliases['dotnet']]
+    else:
+        date_time = _truncate_date_time(date_time, converter.precision)
+        converters = [converter]
+
+    return date_time, converters
+
+
+def _main() -> None:
+    date_time, converters = _parse_args()
+    print(f'Date (UTC): {date_time}')
+
+    for converter in converters:
+        value = converter.from_dt(date_time)
+        print(f'{converter.name}: {value}')
 
 
 if __name__ == '__main__':
